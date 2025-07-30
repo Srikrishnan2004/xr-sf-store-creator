eb57753d80686121a7375eaea6f1b367c010ac59
Saisandeepsangeetham <saisandeep2210495@ssn.edu.in>
2025-07-30 12:07:05 +0530

    Minor Fix - Cloud Url

--- src/api/cloudUtils.ts
+++ src/api/cloudUtils.ts
@@ -35,7 +35,7 @@ export const CLOUD_RUN_ENDPOINTS = {
 
   // Own Store API
   OWN_STORE: {
-    FETCH_PRODUCTS: fn('fetch-products-by-vendor')
+    FETCH_PRODUCTS: fn('fetch-products-vendor-deploy')
   },
 
   // Product Fetch API
